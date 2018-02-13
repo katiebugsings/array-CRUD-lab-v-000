748efcbca66c43ffb77d1972056b9b80a9fe481d
Katie Cassell <kcassell944@gmail.com>
2018-02-13 19:25:03 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,6 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
     index_number = 3
 end
 
